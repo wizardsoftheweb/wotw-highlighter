278fd39b060281b7daa3d62957c2eeee659f32be
CJ Harries <cj@wizardsoftheweb.pro>
2017-11-24 05:33:53 -0600

    Create and test BlockHighlighter's validate

--- tests/test_block_highlighter.py
+++ tests/test_block_highlighter.py
@@ -2,15 +2,42 @@
 # pylint: disable=C0111
 
 from unittest import TestCase
-# from mock import patch
+from mock import patch
 
 from wotw_highlighter import BlockHighlighter
 
 
 class BlockHighlighterTestCase(TestCase):
 
+    def setUp(self):
+        self.build_highlighter()
+
     def wipe_highlighter(self):
+        """Nukes the constructed loader"""
         del self.block_highlighter
 
-    def build_highlighter(self):
-        self.block_highlighter = BlockHighlighter()
+    def build_highlighter(self, *args, **kwargs):
+        """Constructs a basic loader using only the defaults"""
+        # if not kwargs:
+        #     kwargs = self.DEFAULT_KWARGS
+        move_patcher = patch.object(
+            BlockHighlighter, 'move_to_working_directory')
+        move_patcher.start()
+        validate_patcher = patch.object(BlockHighlighter, 'validate')
+        validate_patcher.start()
+        self.block_highlighter = BlockHighlighter(*args, **kwargs)
+        validate_patcher.stop()
+        move_patcher.stop()
+        self.addCleanup(self.wipe_highlighter)
+
+
+class ValidateUnitTests(BlockHighlighterTestCase):
+
+    def test_validate_without_blob(self):
+        self.block_highlighter.blob = None
+        with self.assertRaises(ValueError):
+            self.block_highlighter.validate()
+
+    def test_validate_with_blob(self):
+        self.block_highlighter.blob = 'blob'
+        self.assertIsNone(self.block_highlighter.validate())
--- wotw_highlighter/block_highlighter.py
+++ wotw_highlighter/block_highlighter.py
@@ -1,5 +1,10 @@
 """This file provides a class to highlight code"""
 
+# from pygments import highlight
+# from pygments.lexers import get_lexer_for_filename, guess_lexer
+# from pygments.formatters import HtmlFormatter
+# from pygments.util import ClassNotFound
+
 from wotw_highlighter.block_options import BlockOptions
 
 
@@ -7,3 +12,7 @@ class BlockHighlighter(BlockOptions):
     """
     This class provides a collection of methods to highlight blocks of code
     """
+
+    def validate(self):
+        if not self.blob:
+            raise ValueError('No blob passed to highlighter')
