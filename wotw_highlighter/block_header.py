5f8211fa7c2ea7e8b33a972629edc415c92f67f4
CJ Harries <cj@wizardsoftheweb.pro>
2017-11-25 10:35:52 -0600

    Ignore git_ref_name when HEAD

--- wotw_highlighter/block_header.py
+++ wotw_highlighter/block_header.py
@@ -42,7 +42,8 @@ blob_path and alternate_title cannot both be empty when generating a header\
 
     def render_git_ref_name_tab(self):
         """Renders the VCS branch tab"""
-        if self.git_ref_name:
+        if self.git_ref_name and 'HEAD' != self.git_ref_name:
+            print '"%s"' % (self.git_ref_name)
             return self.construct_code_tab(self.git_ref_name)
         return self.RENDER_AN_OPTION_NOT_INCLUDED
 
