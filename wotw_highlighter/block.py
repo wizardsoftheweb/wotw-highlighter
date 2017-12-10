1c3692110dfb87439a4b69cecc48b9b8d5302ff6
CJ Harries <cj@wizardsoftheweb.pro>
2017-12-10 06:14:05 -0600

    Create highlight method

--- wotw_highlighter/block.py
+++ wotw_highlighter/block.py
@@ -2,6 +2,8 @@
 
 from wotw_highlighter.block_options import BlockOptions
 from wotw_highlighter.block_loader import BlockLoader
+from wotw_highlighter.block_highlighter import BlockHighlighter
+
 
 class Block(BlockOptions):
     """This class loads code, highlights, and returns a polished block"""
@@ -11,3 +13,11 @@ class Block(BlockOptions):
         loaded_block = BlockLoader(**self.full_options())
         loaded_block.load()
         self.update_options(**loaded_block.full_options())
+
+    def highlight(self):
+        """Highlights the block"""
+        highlighted_block = BlockHighlighter(**self.full_options())
+        highlighted_block.attach_lexer()
+        highlighted_block.attach_formatter()
+        highlighted_block.highlight()
+        self.update_options(**highlighted_block.full_options())
